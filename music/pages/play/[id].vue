fa65f2c3ae537d395ba2984253a6d7313e02b919
gochi_AI <63858576+gochiAI@users.noreply.github.com>
2022-06-29 11:58:31 +0900

    Create [id].vue

---
+++ music/pages/play/[id].vue
@@ -0,0 +1,23 @@
+<script setup>
+const router = useRoute().params.id;
+const {data:jofos}=await useFetch('/api/hello');
+const {data:infos}=await useFetch('/api/hello',{pick:[router]})
+</script>
+<template>
+<div>
+<div v-for="i in infos">
+<h3>{{i.name}}</h3>
+<img :src="`../jackets/${i.id}.png`" width="320px" />
+
+<audio :src="`../audios/${i.id}.mp3`" controls />
+<div>
+<a :href="`/play/${i.link[0]}`">
+<img src="../sp.png"/>
+</a>
+<img @click="palupa" :src="operation"/>
+<a :href="`/play/${i.link[1]}`">
+<img src="../sn.png"/>
+</a>
+</div>
+</div>
+</template>
